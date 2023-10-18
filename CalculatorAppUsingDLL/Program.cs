4f48b32cdb2cbfa99e079f17f3c9fa1c9fc91521
Evgeniy Gordienko <112853162+eudk@users.noreply.github.com>
2023-10-18 16:21:09 +0200

    Add project files.

---
+++ CalculatorAppUsingDLL/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CalculatorForDLL; // reference to the DLL namespace
+
+
+    
+        // Create an instance of the math class from the DLL
+        MathOperations mathop = new MathOperations();
+
+        // Perform addition using the math class
+        int result = mathop.Add(5, 3);
+        int result2 = mathop.Subtract(5, 3);
+        int result3 = mathop.Multiply(5, 3);
+
+        double result4 = mathop.Divide(5, 3); // double because divide returns a double
+       // double result5 = mathop.Divide(0, 3); // Any exceptions?
+
+
+// Display the result
+Console.WriteLine("5 + 3 = " + result);
+Console.WriteLine("5 - 3 = " + result2);
+Console.WriteLine("5 * 3 = " + result3);
+Console.WriteLine("5 / 3 = " + result4);
+//Console.WriteLine("0 / 3 = " + result5);
+
+
+    
